84ce848c35da130b3ec64fc0a12241d4246a9b4b
Andrew Wong <wongwong.wong@gmail.com>
2024-07-28 13:48:44 -0600

    feat: login/logout actions

--- src/app.py
+++ src/app.py
@@ -2,18 +2,33 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-from flask import Flask, request
-from flask_cors import CORS
+import flask
+import flask_cors
 from flask import jsonify
+
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
 from personas import asuka, grog, hermione, persona
 from clients.openai_client import OpenAIClient
 from clients.anthropic_client import AnthropicClient
 
+# setup from env
+SESSION_SECRET_KEY = os.environ["SESSION_SECRET_KEY"]
+GOOGLE_OAUTH_CLIENT_ID = os.environ["GOOGLE_OAUTH_CLIENT_ID"]
+ENV = os.getenv("ENV", "local")
 
 # setup flask app
-app = Flask(__name__)
-CORS(app)
+app = flask.Flask(__name__)
+app.secret_key = SESSION_SECRET_KEY
+app.config["MAX_CONTENT_LENGTH"] = 4 * 1000 * 1000  # 4 MB
+
+flask_cors.CORS(app, supports_credentials=True)
+
+if ENV == "gcp":
+    app.config["SESSION_COOKIE_SAMESITE"] = "None"
+    app.config["SESSION_COOKIE_SECURE"] = True
+    app.config["SESSION_COOKIE_DOMAIN"] = "goginko.com"
 
 # setup logging
 if os.getenv("DEBUG"):
@@ -40,9 +55,49 @@ personas = {
 def homepage():
     return 'Ack! What are you doing back here?!'
 
+@app.route("/login", methods=["POST"])
+def login():
+    data = flask.request.json
+    token = data.get("token") if data else None
+
+    # Verify the token here using Google's library or any other method
+    try:
+        idinfo = id_token.verify_oauth2_token(
+            token, requests.Request(), GOOGLE_OAUTH_CLIENT_ID
+        )
+        openid_sub = idinfo["sub"]
+        openid_email = idinfo["email"]
+
+        # create a session! this will create a session token
+        flask.session["openid_sub"] = openid_sub
+        flask.session["openid_email"] = openid_email
+
+        return (
+            flask.jsonify({
+                "message": "Token is valid",
+            }),
+            200,
+        )
+
+    except ValueError as e:
+        return flask.jsonify({"message": str(e)}), 400
+
+@app.route("/check_login", methods=["GET", "POST"])
+def check_login():
+    is_logged_in = flask.session.get("openid_email") is not None
+    return flask.jsonify({"loggedIn": is_logged_in}), 200
+
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    flask.session.clear()  # This clears the entire session
+    return flask.jsonify({"message": "Logged out successfully"}), 200
+
 @app.route('/chat/<persona_name>', methods=['POST'])
 def chat(persona_name):
-    messages = request.json
+    current_user_email = flask.session.get("openid_email")
+
+    messages = flask.request.json
     logger.info(f"{persona_name} - chat input messages: {messages}")
     persona = personas[persona_name]
 
@@ -52,7 +107,7 @@ def chat(persona_name):
         logger.error(e)
         return jsonify({"message": {"content": "<ERROR: CONNECTION INTERRUPTED>", "role": "assistant"}})
 
-    return jsonify({"message": asst_message})
+    return jsonify({"message": asst_message, "email": current_user_email})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
